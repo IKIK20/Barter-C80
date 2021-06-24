92f2326c1d34226a7fa59191ed4900003c198179
IKIK20 <67692637+IKIK20@users.noreply.github.com>
2021-06-24 11:50:53 +0530

    Add files via upload

---
+++ Barter C80/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator} from 'react-navigation';
+
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import { AppDrawerNavigator } from './components/AppDrawerNavigator'
+import { AppTabNavigator } from './components/AppTabNavigator'
+
+
+export default function App() {
+  return (
+    <AppContainer/>
+  );
+}
+
+const switchNavigator = createSwitchNavigator({
+  WelcomeScreen:{screen: WelcomeScreen},
+  Drawer:{screen: AppDrawerNavigator},
+  BottomTab: {screen: AppTabNavigator},
+})
+
+const AppContainer =  createAppContainer(switchNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
